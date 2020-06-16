8e9a7ca2dfebc9458e319f9fba9dabd1278940c9
MichG12 <40402571+MichG12@users.noreply.github.com>
2020-06-16 14:17:18 -0600

    Update memory_6x8.v

--- src/memory_6x8.v
+++ src/memory_6x8.v
@@ -2,7 +2,7 @@
 // Proyecto 2 Digitales II
 
 
-module memory_6x8 #( parameter DATA_SIZE = 8, parameter MAIN_SIZE = 6) (
+module memory #( parameter DATA_SIZE = 8, parameter MAIN_SIZE = 6) (
     input clk,
     input read,       
     input write,       
@@ -29,6 +29,7 @@ module memory_6x8 #( parameter DATA_SIZE = 8, parameter MAIN_SIZE = 6) (
 
     
 endmodule
+
 
 
 
