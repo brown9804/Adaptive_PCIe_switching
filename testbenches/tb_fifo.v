1716e6869a7e10ed2af24e89d14798836d76fa47
MichG12 <40402571+MichG12@users.noreply.github.com>
2020-06-16 13:05:57 -0600

    Update tb_fifo.v

--- testbenches/tb_fifo.v
+++ testbenches/tb_fifo.v
@@ -1,9 +1,11 @@
 `timescale 	1ns	/ 100ps		
 
 `include "./src/fifo.v"
-`include "./syn/fifo_syn.v"
+`include "./syn/fifo_synth.v"
 `include "./testers/t_fifo.v"
 `include "./lib/cmos_cells.v"
+`include "./scr/memory.v"
+`include "-/syn/memory_synth.v"
 module test_bench_fifo;
 
 parameter DATA_SIZE = 10;       
@@ -14,17 +16,21 @@ parameter MAIN_SIZE = 8;
     wire    read;
     wire    write;
     wire [DATA_SIZE-1:0]   data_in_push;           
-    wire [DATA_SIZE-1:0]   almost_full_in;    
-    wire [DATA_SIZE-1:0]   almost_empty_in;
+    wire  almost_full;    
+    wire  almost_empty;
     wire [DATA_SIZE-1:0]   data_out_pop;
     wire    fifo_empty; 
     wire    fifo_error;
     wire    fifo_pause;
-    //Para sintetizado
-    wire fifo_empty_s;       
-    wire [DATA_SIZE-1:0] data_out_pop_s; 
-    wire fifo_error_s;
-    wire fifo_pause_s;
+  //Para sintetizado 
+    wire [DATA_SIZE-1:0] data_out_pop_s;    
+    wire almost_full_s; 
+    wire almost_empty_s;
+    wire fifo_synth_empty;
+    wire fifo_synth_error;
+    wire fifo_synth_pause;
+
+
 
 fifo fifotb (
                  .clk   (clk),
@@ -32,8 +38,8 @@ fifo fifotb (
                  .read (read),
                  .write (write),
                  .data_in_push (data_in_push),
-                 .almost_full_in (almost_full_in),
-                 .almost_empty_in (almost_empty_in),
+                 .almost_full (almost_full),
+                 .almost_empty (almost_empty),
                  .data_out_pop (data_out_pop),
                  .fifo_empty (fifo_empty),
                  .fifo_error (fifo_error),
@@ -46,8 +52,8 @@ fifo_synth fifos (
                  .read (read),
                  .write (write),
                  .data_in_push (data_in_push),
-                 .almost_full_in (almost_full_in),
-                 .almost_empty_in (almost_empty_in),
+                 .almost_full (almost_full_s),
+                 .almost_empty (almost_empty_s),
                  .data_out_pop (data_out_pop_s),
                  .fifo_synth_empty (fifo_empty_s),
                  .fifo_synth_error (fifo_error_s),
@@ -61,16 +67,18 @@ fifo_tester tester (
                  .read (read),
                  .write (write),
                  .data_in_push (data_in_push),
-                 .almost_full_in (almost_full_in),
-                 .almost_empty_in (almost_empty_in),
+                 .almost_full (almost_full),
+                 .almost_empty (almost_empty),
                  .data_out_pop (data_out_pop),
                  .fifo_empty (fifo_empty),
                  .fifo_error (fifo_error),
                  .fifo_pause (fifo_pause),
+                 .almost_full_s (almost_full_s),
+                 .almost_empty_s (almost_empty_s),
                  .data_out_pop_s (data_out_pop_s),
-                 .fifo_empty_s (fifo_empty_s),
-                 .fifo_error_s (fifo_error_s),
-                 .fifo_pause_s (fifo_pause_s)
+                 .fifo_synth_empty (fifo_synth_empty),
+                 .fifo_synth_error (fifo_synth_error),
+                 .fifo_synth_pause (fifo_synth_pause_)
 );
 
 
