e3d2dbc5c798085eaa12a1c92f0df2a05774c6d2
MichG12 <40402571+MichG12@users.noreply.github.com>
2020-06-16 14:21:28 -0600

    Add files via upload

---
+++ testers/t_fifo_6x8.v
@@ -0,0 +1,111 @@
+// FIFO 6x8 tester
+// Proyecto 2 Digitales II
+
+//`include "fifo_synth.v"
+//`include "fifo.v"
+
+module fifo_tester #(
+    //Parametros
+    parameter DATA_SIZE = 8,         
+    parameter MAIN_SIZE=6          
+)(
+    input wire fifo_empty,        
+    input wire [DATA_SIZE-1:0] data_out_pop,    
+    input wire fifo_error,
+    input wire fifo_pause,
+    input wire  almost_full,   
+    input wire  almost_empty,
+    output reg clk,
+    output reg reset,
+    output reg read,
+    output reg write,
+    output reg [DATA_SIZE-1:0] data_in_push,           
+    // Para sintetizado
+    input wire fifo_synth_empty,        
+    input wire [DATA_SIZE-1:0] data_out_pop_s,    
+    input wire fifo_synth_error,
+    input wire fifo_synth_pause,
+    input wire  almost_full_s,   
+    input wire almost_empty_s
+);
+ 
+
+ initial begin
+
+    $dumpfile("fifo_6x8_c_s.vcd");        
+    $dumpvars;
+    write ='b0;
+    read ='b0;
+    data_in_push ='b0;
+    reset = 'b0;                             
+    read  = 'b0;
+    write = 'b0;
+
+    #4;
+    @(posedge clk)
+    reset <= 	'b1;
+    data_in_push <= 'h3;
+    
+    repeat(6)begin
+        @(posedge clk)
+        data_in_push <= data_in_push+1;
+    end
+
+    
+    @(posedge clk)
+    data_in_push <= data_in_push+1;
+    repeat(12)begin
+        @(posedge clk)
+        write <= 1;     
+        data_in_push <= data_in_push + 1;
+        
+    end
+
+    
+    @(posedge clk)
+        write<=0;
+        // read <= 1;
+        
+
+    repeat(2)begin
+        @(posedge clk)
+        //mandar direcciones random a wr_ptr
+        read <= 1;
+    end
+        
+    repeat(2)begin
+        @(posedge clk)
+            write<=0;
+            read <= 0;
+    end
+
+    repeat(4)begin
+        @(posedge clk)
+            //mandar direcciones random a wr_ptr
+            read<=0; 
+    end
+
+    repeat(7)begin
+        @(posedge clk)
+            read <= 1;
+    end
+    @(posedge clk)
+        read <= 1;
+
+    // @(posedge clk)
+    //     //mandar direcciones random a wr_ptr
+    //     read<=1;
+
+    repeat(4)begin
+        @(posedge clk)
+        read<=1;
+        write <= 0;
+        // data_in_push<='h4;
+    end
+
+    $finish;
+end
+    initial clk <= 0;
+    always # 2 clk <= ~clk;       //genera señal 4 ns 
+
+endmodule
---
+++ testers/t_memory_6x8.v
@@ -0,0 +1,71 @@
+// Memoria 6x8 tester
+// Proyecto 2 Digitales II
+
+module memory_tester #( parameter MAIN_SIZE=6, parameter DATA_SIZE = 8)(
+	output reg  clk, 
+    output reg	read,         
+    output reg  write,      
+	output reg  reset,
+	output reg [MAIN_SIZE-1:0] wr_ptr,
+	output reg [MAIN_SIZE-1:0] rd_ptr,
+	output reg [DATA_SIZE-1:0] data_in,
+	input  [DATA_SIZE-1:0] data_out,
+// Para el sintetizado
+    input [DATA_SIZE-1:0] data_out_s
+);
+
+ initial begin
+
+    $dumpfile("memory_6x8_con_sin.vcd");       
+    $dumpvars;
+    data_in = 'b0;
+    wr_ptr  = 'b0;					
+    rd_ptr  = 'b0;                    
+    reset = 'b0;                            
+    read = 'b0;
+    write = 'b0;
+
+  
+    @(posedge clk)
+    reset <= 1;     
+    write <= 1;
+    
+    repeat(30)begin
+        @(posedge clk)
+        data_in <= data_in + 1;
+        wr_ptr <= wr_ptr + 1;
+    end
+        # 25;
+
+        @(posedge clk)
+        read <= 1;
+
+    repeat(30)begin
+        @(posedge clk)
+        data_in <= data_in + 1;
+        rd_ptr <= rd_ptr + 1;
+        wr_ptr <= wr_ptr + 1;
+    end
+        
+
+    # 18;
+    @(posedge clk)
+    data_in   <= data_in + 1;
+    write     <= 0;
+
+    repeat(16)begin  
+        @(posedge clk)
+        data_in   <= data_in + 1;
+        rd_ptr <= rd_ptr + 1;
+    end
+
+    @(posedge clk)
+    reset <= 0;
+    #20
+
+    $finish;
+end
+    initial clk <= 0;
+    always # 2 clk <= ~clk;    
+
+endmodule
