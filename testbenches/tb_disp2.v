ff34ba79b5fa28f243a6c1e10032671a0b6cd474
MichG12 <40402571+MichG12@users.noreply.github.com>
2020-06-24 11:04:13 -0600

    Add files via upload

---
+++ testbenches/tb_disp2.v
@@ -0,0 +1,53 @@
+// Probador Disp 2
+// Proyecto 2 - Circuitos Digitales 2
+// Michelle Gutiérrez
+
+`include "./scr/disp2.v"
+`include "./testers/t_disp2.v"
+
+module testbench_disp2;
+
+wire in1;
+wire in2;
+wire reset;
+wire clk;
+wire clk8f;
+wire read;
+wire write;
+wire [7:0] out1;
+wire [7:0] out2;
+wire almost_full_f1;
+wire almost_full_f2;
+
+
+disp2 dis2 (
+              .in1 (in1),
+              .in2 (in2),
+              .reset (reset),
+              .clk   (clk),
+              .clk8f (clk8f),
+              .read  (read),
+              .write (write),
+              .out1  (out1),
+              .out2  (out2),
+              .almost_full_f1 (almost_full_f1),
+              .almost_full_f2 (almost_full_f2)
+);
+
+disp2_tester tester (
+              .in1 (in1),
+              .in2 (in2),
+              .reset (reset),
+              .clk   (clk),
+              .clk8f (clk8f),
+              .read  (read),
+              .write (write),
+              .out1  (out1),
+              .out2  (out2),
+              .almost_full_f1 (almost_full_f1),
+              .almost_full_f2 (almost_full_f2)
+);
+
+
+
+endmodule
