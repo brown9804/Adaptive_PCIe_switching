cc144bab56912ecdd9c612411edda69dd39d6514
Belinda Brown <belindabrownr04@gmail.com>
2020-07-01 13:04:08 -0600

    SRC disp3 possible changes B-B M0 🤔 --> tester

--- src/disp3.v
+++ src/disp3.v
@@ -25,11 +25,13 @@ input wire clk,
 input wire clk8f,
 input wire reset,
 input wire [DATA_SIZE-1:0] in,
-
-
+input wire read0, // need to connect with df control and fifo0
+input wire read1, // need to connect with df control and fifo1
+input wire write0, // need to connect with write and fifo0
+input wire write1, // need to connect with write and fifo1
 //Outputs
-output reg out0, // out from fifo4x8 #0
-output reg out1,  // out from fifo4x8 #1
+output reg [MAIN_SIZE-1:0] out0, // out from fifo4x8 #0
+output reg [MAIN_SIZE-1:0] out1,  // out from fifo4x8 #1
 output reg Error0, // Error fifo4x8 #0
 output reg Error1 // Error fifo4x8 #1
 
@@ -53,43 +55,51 @@ output reg fifo1_pause,*/
 // // wires internally
 wire outF0, outF1;
 wire Error_F0, Error_F1;
-wire [7:0] out0_disp3, out1_disp3;
-wire nn_alf1, nn_alf2; // almost full 1 and 2
-wire nn_read, nn_write; // for write and write
+wire [MAIN_SIZE-1:0] out0_disp3, out1_disp3;
+wire nn_alf0, nn_alf1; // almost full 1 and 2
+wire nn_empty0, nn_empty1; // empty 1 and 2
+// wire nn_read0, nn_write0; // for write and write
+// wire nn_read1, nn_write1; // for write and write
 
-device1  device1_in3(/*AUTOINST*/
+device1  #( .DATA_SIZE(DATA_SIZE), .MAIN_SIZE(MAIN_SIZE) )
+ device1_in3(/*AUTOINST*/
   //Outputs
-  .out0 (outF0), // out from fifo6x8 #0 // output 1 bit
-  .out1 (outF1),  // out from fifo6x8 #1 // output 1 bit
+  .out0 (outF0), // out from fifo6x8 #0
+  .out1 (outF1),  // out from fifo6x8 #1
   .Error_class (Error_F0),
   .Error_route (Error_F1),
   //INPUTS
   .clk (clk),
   .clk8f (clk8f),
-
   .reset(reset),
-  .in (in) // input 10 bits
+  .in (in),
+  .fifo0_disp2_almostfull (nn_alf0),
+  .fifo1_disp2_almostfull (nn_alf1)
 );
 
 
-
-device2 device2_in3(/*AUTOINST*/
+//// inital par 8 main size and 10 data size
+//// 4 main size 8 data size
+device2  #( .DATA_SIZE(DATA_SIZE-2), .MAIN_SIZE(MAIN_SIZE-4) )
+device2_in3(/*AUTOINST*/
 // Outputs
-.out1 (out0_disp3),  // output one bit
-.out2 (out1_disp3), // output one bit
+.out0 (out0_disp3),
+.out1 (out1_disp3),
+.almost_full_f0 (nn_alf0),
 .almost_full_f1 (nn_alf1),
-.almost_full_f2 (nn_alf2),
+.empty0 (nn_empty0),
+.empty1 (nn_empty1),
 // Inputs
-.in1 (outF0),  // input one bit
-.in2 (outF1), // input one bit
+.in0 (outF0),
+.in1 (outF1),
 .reset (reset),
 .clk (clk),
 .clk8f (clk8f),
-.read (nn_read),
-.write (nn_write)
+.read0 (read0),
+.read1 (read1),
+.write0 (write0),
+.write1 (write1)
 );
-
-
 
   always@(*) begin      // pass to outputs
     out0 = out0_disp3;
